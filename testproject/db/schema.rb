fab2c0115e66448869df346a8bfc234717d9bcbd
masinag <gabriele.masina@virgilio.it>
2016-07-24 11:03:27 +0200

    Added 'published' and 'word_count fields to articles'

--- testproject/db/schema.rb
+++ testproject/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160721083355) do
+ActiveRecord::Schema.define(version: 20160724090150) do
 
   create_table "articles", force: :cascade do |t|
     t.string   "title"
@@ -19,6 +19,8 @@ ActiveRecord::Schema.define(version: 20160721083355) do
     t.string   "category"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean  "published"
+    t.integer  "word_count"
   end
 
 end
